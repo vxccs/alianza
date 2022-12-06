acd7e66178aa3ba8bc4419e95e5c90017e50e90a
vxccs <vicc1997@gmail.com>
2022-12-06 13:10:08 -0400

    add superb and amatic

---
+++ amatic/index.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('#mobil-menu');
+const menu = document.querySelector('.mobile-dropdown');
+
+btnMenu.addEventListener('click', (e) => {
+  menu.classList.toggle('show-menu');
+  btnMenu.classList.toggle('open-icon');
+});
---
+++ superB/index.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector('#mobil-menu');
+const menu = document.querySelector('.mobile-dropdown');
+
+btnMenu.addEventListener('click', (e) => {
+  menu.classList.toggle('show-menu');
+});
