d0515e85c7c95f0d41e29e7b1f20c0646e6dd181
vxccs <vicc1997@gmail.com>
2022-12-14 14:53:43 -0400

    fix everything

---
+++ nosotros/index.js
@@ -0,0 +1,12 @@
+const btnMenu = document.querySelector('#mobil-menu');
+const menu = document.querySelector('.mobile-dropdown');
+
+btnMenu.addEventListener('click', (e) => {
+  menu.classList.toggle('show-menu');
+});
+
+document.addEventListener('click', (e) => {
+  if (!menu.contains(e.target) && !btnMenu.contains(e.target)) {
+    menu.classList.remove('show-menu');
+  }
+});
